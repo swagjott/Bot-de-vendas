942df09de30dfb2b2fd26baa66e61c856f5cd13b
jꝋttzx愛 <147317692+swagjott@users.noreply.github.com>
2025-10-13 00:48:56 -0300

    Create interactionCreate.js

---
+++ events/interactionCreate.js
@@ -0,0 +1,15 @@
+export default {
+  name: 'interactionCreate',
+  async execute(interaction, client) {
+    if (interaction.isCommand()) {
+      const command = client.commands.get(interaction.commandName);
+      if (!command) return;
+      try {
+        await command.execute(interaction, client);
+      } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'Ocorreu um erro!', ephemeral: true });
+      }
+    }
+  }
+};
